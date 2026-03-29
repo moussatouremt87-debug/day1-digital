66d67d81af3b88f03cc409266dd686216504d547
moussatouremt87-debug <moussatoure.mt.87@gmail.com>
2026-03-29 14:44:43 +0400

    feat: add CGV page SEO metadata layout

---
+++ app/cgv/layout.js
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Conditions G\u00e9n\u00e9rales de Vente',
+  description:
+    'Consultez les Conditions G\u00e9n\u00e9rales de Vente de Day1.digital. Transparence totale sur nos prestations, d\u00e9lais, tarifs et garanties.',
+  alternates: {
+    canonical: 'https://www.day1-digital.com/cgv',
+  },
+  openGraph: {
+    title: 'CGV \u2014 Day1.digital',
+    description:
+      'Conditions G\u00e9n\u00e9rales de Vente de Day1.digital. Prestations, d\u00e9lais, tarifs et garanties.',
+    url: 'https://www.day1-digital.com/cgv',
+  },
+};
+
+export default function CGVLayout({ children }) {
+  return children;
+}
